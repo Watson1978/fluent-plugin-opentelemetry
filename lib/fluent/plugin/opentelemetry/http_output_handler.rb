e23600295ee146090d340b6c9d22441cc22135b8
Shizuo Fujita <fujita@clear-code.com>
2026-01-24 03:07:48 +0900

    Remove connection reset

--- lib/fluent/plugin/opentelemetry/http_output_handler.rb
+++ lib/fluent/plugin/opentelemetry/http_output_handler.rb
@@ -56,8 +56,6 @@ class Fluent::Plugin::Opentelemetry::HttpOutputHandler
     else
       @logger.error "got error response from '#{uri}', response code is #{response.status}"
     end
-  ensure
-    connection.reset
   end
 
   private
